0e3ef9f2f6cd58654b4e81918decdcf5a5d93e5c
kanchan110295 <kprawat@ncsu.edu>
2022-09-04 17:59:27 -0400

    Added constructor for Num

---
+++ code/Num.py
@@ -0,0 +1,17 @@
+import math
+
+class Num:
+
+    # `Num` summarizes a stream of numbers.
+
+    def __init__(self,c=0,s="",w=""):
+        self.n=0
+        self.at=c
+        self.name=s 
+        self._has={}
+        self.lo= math.inf   # lowest seen
+        self.hi= -math.inf  # highest seen
+        self.isSorted=True # No updates since last sort of data
+        self.w= (s.find('-$')+1 and -1 or 1)
+        
+        
