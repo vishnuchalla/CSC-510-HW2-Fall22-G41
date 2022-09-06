aba19deb51371df449303db9bdeef232dcda5b89
Srujan Ponnur <srujanponnur@gmail.com>
2022-09-05 22:11:39 -0400

    removed pytest and changed the return type for all methods

--- test/test_Sym.py
+++ test/test_Sym.py
@@ -1,19 +1,4 @@
-import pytest
 from code.Sym import Sym
-
-"""
-Fixture to setup and teardown sym object for tests.
-"""
-
-
-@pytest.fixture()
-def sym_test():
-    print("\nSetting up sym object for test")
-    sym = Sym()
-    yield sym
-    print("\nDeleting sym object as a teardown")
-    del sym
-
 
 """
 Class to test Sym.
@@ -24,42 +9,41 @@ class TestSym:
     """
     Method to test add method.
     """
-    def test_add(self, sym_test):
-        sym_test.add('Volume')
-        assert sym_test.n == 1
-        assert sym_test._has['Volume'] == 1
+    def test_add(self):
+        sym = Sym()
+        sym.add('Volume')
+        return sym.n == 1 and sym._has['Volume'] == 1
 
     """
     Method to test the mid method -- This tests the most frequent column in _has dictionary
     """
-    def test_mid(self, sym_test):
-        sym_test.add('Volume')
-        sym_test.add('Volume')
-        sym_test.add('Clndrs')
-        assert sym_test.mid(None,None,None) == 'Volume'
+    def test_mid(self):
+        sym = Sym()
+        sym.add('Volume')
+        sym.add('Volume')
+        sym.add('Clndrs')
+        return sym.mid(None, None, None) == 'Volume'
 
     """
     Method to test the div method
     """
-    def test_div(self, sym_test):
-        sym_test.add('Volume')
-        sym_test.add('Volume')
-        sym_test.add('Clndrs')
-        assert sym_test.div(0,None) == pytest.approx(0.91, 0.08)
+    def test_div(self):
+        sym = Sym()
+        sym.add('Volume')
+        sym.add('Volume')
+        sym.add('Clndrs')
+        return round(sym.div(0, None), 2) == 0.92
 
     """
     To test add, mid and div methods
     """
-    def test_sym(self, sym_test, entropy=None, mode=None):
+    def test_sym(self, entropy=None, mode=None):
+        sym = Sym()
         for i in ['a', 'a', 'a', 'a', 'b', 'b', 'c']:
-            sym_test.add(i)
-        mode, entropy = sym_test.mid(None, None, None), sym_test.div(0, None)
+            sym.add(i)
+        mode, entropy = sym.mid(None, None, None), sym.div(0, None)
         entropy = ((1000 * entropy) // 1) / 1000
-        assert mode == 'a'
-        assert 1.38 >= entropy >= 1.37
-
-
-
+        return mode == 'a' and 1.38 >= entropy >= 1.37
 
 
 
