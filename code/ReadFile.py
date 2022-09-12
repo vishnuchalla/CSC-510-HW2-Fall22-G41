58ca24298bd4bdc9d2bffb68f3b45b3334d84f3f
Cholo <sumanthbs@Sumanths-MBP.lan>
2022-09-12 16:54:27 -0400

    Adding ReadFile.py

---
+++ code/ReadFile.py
@@ -0,0 +1,29 @@
+import math
+import random
+import collections
+
+def __init__(self):
+    pass
+
+def csv(self,fname,fun,sep,src,s,t):
+    src = fname
+    while true:
+        with open(src) as f:
+            s = f.read()
+            if s:
+                t = {}
+                for s1 in s.split(','):
+                    t[1+len(t)] = coerce(s1)
+                fun(t)
+
+
+def coerce(s,fun):
+    def fun(s1):
+        if s1:
+            return true
+        else:
+            return false
+        return s1
+    return s.isnumeric()
+
+ 
