b78a23ee2e07f99ddc82ec43a570d8f86c1577ae
svnsairam <svnsairam.sakhamuri@gmail.com>
2022-09-12 17:17:07 -0400

    Added the test data file

--- test/test_Data.py
+++ test/test_Data.py
@@ -4,3 +4,21 @@ from code.PrettyPrint import PrettyPrint
 """
 Class to test data methods in the package.
 """
+class TestData(object):
+    
+    """
+    Method to read the csv file
+    """
+    def test_readcsv(self):
+        n=0
+        with open('data/auto93.csv', 'r') as file:
+            my_reader = csv.reader(file)
+            for row in my_reader:
+                print(row)
+                n+=1
+                if n > 10:
+                    return
+                else:
+                    prettyPrint = PrettyPrint()
+                    prettyPrint.oo(row)
+        return True
