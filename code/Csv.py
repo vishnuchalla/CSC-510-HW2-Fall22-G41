0d48c76ee7906e593feb5ae39e33c0882cd8a782
kanchan110295 <kprawat@ncsu.edu>
2022-09-12 23:15:38 -0400

    Modified csv file

--- code/Csv.py
+++ code/Csv.py
@@ -2,6 +2,7 @@ import math
 import random
 import collections
 import re
+import csv
 
 """
 This method needs to be corrected later once we understand the data better.
@@ -9,13 +10,16 @@ This method needs to be corrected later once we understand the data better.
 def csv(self,fname,fun,sep=None,src=None,s=None,t=None):
     sep = ","
     src = fname
-    while True:
-        with open(src) as f:
-            s = f.read()
-            if s:
-                t = {}
-                for s1 in s.split(','):
-                    t[1+len(t)] = coerce(s1)
+    #while True:
+    #    with open(src) as f:
+    #        s = f.read()
+    #        if s:
+    #            t = {}
+    #            for s1 in s.split(','):
+    #                t[1+len(t)] = coerce(s1)
+    with open('data/auto93.csv', 'r') as file:
+        my_reader = csv.reader(file)
+    return my_reader
 
 
 def coerce(s,fun=None):
