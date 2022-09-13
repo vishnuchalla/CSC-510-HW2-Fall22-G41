847fcce64fcc9e6b141f5b952da0dc4fce1da6c7
kanchan110295 <kprawat@ncsu.edu>
2022-09-13 00:31:42 -0400

    Added Csv class

--- code/Csv.py
+++ code/Csv.py
@@ -7,22 +7,22 @@ import csv
 """
 This method needs to be corrected later once we understand the data better.
 """
-def csv(self,fname,fun,sep=None,src=None,s=None,t=None):
-    sep = ","
-    src = fname
-    #while True:
-    #    with open(src) as f:
-    #        s = f.read()
-    #        if s:
-    #            t = {}
-    #            for s1 in s.split(','):
-    #                t[1+len(t)] = coerce(s1)
-    with open('data/auto93.csv', 'r') as file:
-        my_reader = csv.reader(file)
-    return my_reader
+class Csv:
+    def csv(self,fname,fun,sep=None,src=None,s=None,t=None):
+        sep = ","
+        src = fname
+        #while True:
+        #    with open(src) as f:
+        #        s = f.read()
+        #        if s:
+        #            t = {}
+        #            for s1 in s.split(','):
+        #                t[1+len(t)] = coerce(s1)
+        with open('data/auto93.csv', 'r') as file:
+            my_reader = csv.reader(file)
+        return my_reader
 
-
-def coerce(s,fun=None):
-    return (False if not re.match(s, "^%s*(.-)%s*$") else True) or int(s)
+    def coerce(s,fun=None):
+        return (False if not re.match(s, "^%s*(.-)%s*$") else True) or int(s)
 
  
