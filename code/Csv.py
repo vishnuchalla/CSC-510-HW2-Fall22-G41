e7f6d3d2e7924c6eff9968f7993fd1a49b31a614
kanchan110295 <kprawat@ncsu.edu>
2022-09-13 01:24:13 -0400

    Rolled back to previous version

--- code/Csv.py
+++ code/Csv.py
@@ -2,13 +2,12 @@ import math
 import random
 import collections
 import re
-import csv
 
 """
 This method needs to be corrected later once we understand the data better.
 """
-class Csv:
-    def csv(self,fname,fun,sep=None,src=None,s=None,t=None):
+
+def csv(self,fname,fun,sep=None,src=None,s=None,t=None):
         sep = ","
         src = fname
         while True:
@@ -19,12 +18,7 @@ class Csv:
                     for s1 in s.split(','):
                         t[1+len(t)] = coerce(s1)
         
-    def csv(self):
-        with open('data/auto93.csv', 'r') as file:
-            my_reader = csv.reader(file)
-        return my_reader
-
-    def coerce(s,fun=None):
+def coerce(s,fun=None):
         return (False if not re.match(s, "^%s*(.-)%s*$") else True) or int(s)
 
  
